d242812c04fb269347ab909dfdb7db94ea7fca5c
Aashish Raj <aashish10raj@gmail.com>
2023-09-07 16:48:52 +0530

    all issues fixed

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -16,32 +16,6 @@ class ShowForm(Form):
         validators=[DataRequired()],
         default= datetime.today()
     )
-class Genre(Enum):
-    Alternative = 'Alternative'
-    Blues = 'Blues'
-    Classical = 'Classical'
-    Country = 'Country'
-    Electronic = 'Electronic'
-    Folk = 'Folk'
-    Funk = 'Funk'
-    Hip_Hop = 'Hip-Hop'
-    Heavy_Metal = 'Heavy Metal'
-    Instrumental = 'Instrumental'
-    Jazz = 'Jazz'
-    Musical_Theatre = 'Musical Theatre'
-    Pop = 'Pop'
-    Punk = 'Punk'
-    R_n_B = 'R&B'
-    Reggae = 'Reggae'
-    Rock_n_Roll = 'Rock n Roll'
-    Soul = 'Soul'
-    Other = 'Other'
-def get_genre_choices(enum):
-    choices = []
-    for genre in enum:
-        choices.append((genre.name, genre.value))
-    return choices
-
 
 class VenueForm(Form):
     name = StringField(
@@ -118,7 +92,27 @@ class VenueForm(Form):
     genres = SelectMultipleField(
         # TODO implement enum restriction
         'genres', validators=[DataRequired()],
-        choices= get_genre_choices(Genre)
+        choices= [
+            ('Alternative', 'Alternative'),
+            ('Blues', 'Blues'),
+            ('Classical', 'Classical'),
+            ('Country', 'Country'),
+            ('Electronic', 'Electronic'),
+            ('Folk', 'Folk'),
+            ('Funk', 'Funk'),
+            ('Hip-Hop', 'Hip-Hop'),
+            ('Heavy Metal', 'Heavy Metal'),
+            ('Instrumental', 'Instrumental'),
+            ('Jazz', 'Jazz'),
+            ('Musical Theatre', 'Musical Theatre'),
+            ('Pop', 'Pop'),
+            ('Punk', 'Punk'),
+            ('R&B', 'R&B'),
+            ('Reggae', 'Reggae'),
+            ('Rock n Roll', 'Rock n Roll'),
+            ('Soul', 'Soul'),
+            ('Other', 'Other'),
+        ]
     )
     facebook_link = StringField(
         'facebook_link', validators=[URL()]
@@ -207,7 +201,27 @@ class ArtistForm(Form):
     )
     genres = SelectMultipleField(
         'genres', validators=[DataRequired()],
-        choices=get_genre_choices(Genre)
+        choices=[
+            ('Alternative', 'Alternative'),
+            ('Blues', 'Blues'),
+            ('Classical', 'Classical'),
+            ('Country', 'Country'),
+            ('Electronic', 'Electronic'),
+            ('Folk', 'Folk'),
+            ('Funk', 'Funk'),
+            ('Hip-Hop', 'Hip-Hop'),
+            ('Heavy Metal', 'Heavy Metal'),
+            ('Instrumental', 'Instrumental'),
+            ('Jazz', 'Jazz'),
+            ('Musical Theatre', 'Musical Theatre'),
+            ('Pop', 'Pop'),
+            ('Punk', 'Punk'),
+            ('R&B', 'R&B'),
+            ('Reggae', 'Reggae'),
+            ('Rock n Roll', 'Rock n Roll'),
+            ('Soul', 'Soul'),
+            ('Other', 'Other'),
+        ]
      )
     facebook_link = StringField(
         # TODO implement enum restriction
